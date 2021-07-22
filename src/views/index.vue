8289f4db9daa1c37d81dacf188e80f4af414e444
qijiao <1003297709@qq.com>
2021-07-22 09:23:33 +0800

    样式

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,15 +1,28 @@
 <template>
-  <div>
-    123
+  <div class="education_content">
+    <dv-full-screen-container>
+      <education-head />
+    </dv-full-screen-container>
   </div>
 </template>
 
 <script>
+import educationHead from './education/educationHead'
 export default {
-
+  components: {
+    educationHead
+  }
 }
 </script>
 
 <style lang="less">
+@import '~@/assets/css/variables.less';
+.education_content{
+  width: 100%;
+  height: 100%;
+  // background-color: @eMain;
+  background: url('../assets/1.jpg') @eMain no-repeat center center;
+  font-size: 14px;
+}
 
 </style>
